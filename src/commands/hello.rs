ac43b9740339793994c024c7ef37f66692508bc0
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2023-03-14 20:34:42 +1100

    Experiment with slash command using serenity examples

---
+++ src/commands/hello.rs
@@ -0,0 +1,10 @@
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::interaction::application_command::CommandDataOption;
+
+pub fn run(_options: &[CommandDataOption]) -> String {
+    "Hello NEIN from Rust!".to_string()
+}
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command.name("hello").description("NEIN NEIN NEIN command")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,79 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpStream, TcpListener}, fmt::format, thread, time::Duration,
+    net::{TcpStream}, thread, time::Duration,
 };
-use rustobot::ThreadPool;
 
-fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:7878") {
-        Ok(res) => res,
-        Err(r) => panic!("{}", r)
-    };
+use dotenv::dotenv;
+use std::env;
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::gateway::Ready;
 
-    let pool = ThreadPool::new(4);
+mod commands;
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::id::GuildId;
 
-        println!("Shutting down.");
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "hello" => commands::hello::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| commands::hello::register(command))
+        })
+        .await;
+
+        println!("I now have the following guild slash commands: {:#?}", commands);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("CLIENT_TOKEN").expect("Expected a token in the environment");
+
+    // Build client.
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
     }
 }
 
